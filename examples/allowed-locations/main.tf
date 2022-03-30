8b6219f9b2060daed5db62220fa5129b90be2403
Daniel Liang (eHealth NSW) <Daniel.Liang@health.nsw.gov.au>
2022-03-30 18:54:18 +1100

    first

--- examples/allowed-locations/main.tf
+++ examples/allowed-locations/main.tf
@@ -3,6 +3,6 @@ data "azurerm_subscription" "subscription" {
 }
 
 module "policy" {
-  source                      = "git::https://github.com/daniel-liang/tfmodule.git"
+  source                      = "git::https://github.com/daniel-liang/tfmodule.git?ref=v1.0.0"
   policy_definition_file_path = format("%s/%s", path.module, "allowed-location-policy.json")
 }
